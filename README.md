d8d93693d5334bb6412a2be822a59bbbe69c4055
Ihwan <mynameisihwan@gmail.com>
2020-05-17 15:33:56 +0700

    create rice cooker hack playground project

---
+++ README.md
@@ -0,0 +1,33 @@
+## WWDC20 Swift Student Challenge - Rice Cooker Hack
+
+<p align=center>
+    <a href="https://developer.apple.com/wwdc20/"><img alt="WWDC" src="https://img.shields.io/badge/WWDC-2020-121b2e.svg"></a>
+    <a href="https://developer.apple.com/wwdc20/swift-student-challenge/"><img alt="Status" src="https://img.shields.io/badge/Status-submitted-blue.svg"></a>
+</p>
+
+![intro](screenshot/intro.png)
+![main](screenshot/main.png)
+![result](screenshot/result.png)
+
+### [Demo](https://youtu.be/0fgdYEAn6MQ)
+
+### Requisites
+
+* MacOS 10.15.4
+* Xcode 11.4.1
+
+### Introduction
+
+This is the playground I submitted for WWDC20 Swift Student Challenge. It's also my first time applying for WWDC Submission.
+
+I spend about four full days to complete this playground. I give my playground the name “Rice Cooker Hack", because this playground illustrates how to cook using minimum ingredients and maximize rice cooker. I got this idea since i am living in the room with only rice cooker that can i use to cook.
+
+### Description
+
+The topic of my Playground is "What can rice cooker do?". Some people know that rice cooker can cook rice, but only a few people know that rice cooker can cook a lot of meals. The aim of this Playground is help people to experiment with ingredients and combine it become meal. if the ingredients is right the alert with food name and fun sound effect will appear, if not you can try again. 
+
+### Third party resource used:
+
+- Music background: Goat by Wayne Jones from [Youtube Audio Library](https://www.youtube.com/audiolibrary/music). The sound is good and represent joyful when playing game. i used it as my background music.
+- Sound Effect: click, kids cheering and failed sound effect from [Zedge](https://zedge.net). the sounds effect will make my game more cheerful and interactive. i used the sound effect for audio feedback when user click ingredients card & cook button. 
+- Icon : All the icon in this game come from [Flaticon]( www.flaticon.com), with some author like [Monkik](https://www.flaticon.com/authors/monkik), [Freepik](https://www.flaticon.com/authors/freepik) and [Pixel Perfect](https://www.flaticon.com/authors/pixel-perfect)
---
+++ ricecookerhack.playground/Contents.swift
@@ -0,0 +1,266 @@
+import PlaygroundSupport
+import SwiftUI
+import AVFoundation
+
+struct ContentView: View{
+    
+    @State var ingredients: [String] = ["rice","noodle", "egg", "kale", "margarine", "tomato", "macaroni", "water", "jelly", "seasoning", "ketchup","milk"]
+    @State var selections: [String] = []
+    
+    @State private var showingAlert = false
+    @State private var isAudio = false
+    @State private var result: String? = nil
+    @State private var isCorrect = false
+    
+    var body: some View {
+        
+        ZStack{
+            Rectangle()
+                .foregroundColor(Color(red: 200/255, green: 143/255, blue: 32/255)).edgesIgnoringSafeArea(.all)
+            
+            VStack{
+                
+                Text("RiceCooker Hack").font(.largeTitle).padding(.top, 100).foregroundColor(Color.white)
+                
+                Button(action: {
+                    Sounds.playBackSound(soundfile: "Goat.mp3")
+                    self.isAudio = true
+                }){
+                    Image(uiImage: UIImage(named: isAudio ? "soundon.png" : "soundoff.png")!).renderingMode(.original)
+                        .resizable().frame(width: 40,height: 40)
+                        .aspectRatio(contentMode: .fit)
+                }.padding(.vertical, 20)
+                
+                
+                GridStack(rows: 3, columns: 4) { row, col in
+                    CardView(name: self.ingredients[row * 4 + col], isSelected: self.selections.contains(self.ingredients[row * 4 + col])){
+                        if self.selections.contains(self.ingredients[row * 4 + col]) {
+                            self.selections.removeAll(where: { $0 == self.ingredients[row * 4 + col] })
+                            Sounds.playSounds(soundfile: "click.mp3")
+                            
+                        }
+                        else {
+                            self.selections.append(self.ingredients[row * 4 + col])
+                            Sounds.playSounds(soundfile: "click.mp3")
+                        }
+                    }
+                }
+                
+                Button(action: {
+                    let nasgor = ["egg","margarine","rice", "seasoning"]
+                    let rice = ["rice","water"]
+                    let omelet = ["margarine", "egg", "seasoning"]
+                    let sautedKale = ["seasoning", "kale", "margarine"]
+                    let jelly = ["jelly","water"]
+                    let macharoni = ["macaroni", "seasoning", "water"]
+                    let ramen = ["noodle", "egg", "water", "seasoning"]
+                    
+                    if self.selections.count < 2{
+                        Sounds.playSounds(soundfile: "fail.mp3")
+                    }else{
+                        if self.selections.containsSameElements(as: nasgor) {
+                            Sounds.playSounds(soundfile: "kids.mp3")
+                            self.isCorrect = true
+                            self.result = "nasi goreng"
+                        }else if self.selections.allSatisfy(rice.contains){
+                            self.isCorrect = true
+                            self.result = "rice"
+                            Sounds.playSounds(soundfile: "kids.mp3")
+                            
+                        }else if self.selections.allSatisfy(omelet.contains){
+                            self.isCorrect = true
+                            self.result = "Omelet"
+                            Sounds.playSounds(soundfile: "kids.mp3")
+                            
+                        }else if self.selections.allSatisfy(sautedKale.contains){
+                            self.isCorrect = true
+                            self.result = "Sauted Kale"
+                            Sounds.playSounds(soundfile: "kids.mp3")
+                            
+                        }else if self.selections.allSatisfy(jelly.contains){
+                            self.isCorrect = true
+                            self.result = "Jelly"
+                            Sounds.playSounds(soundfile: "kids.mp3")
+                        }else if self.selections.allSatisfy(macharoni.contains){
+                            self.isCorrect = true
+                            self.result = "Macharoni"
+                            Sounds.playSounds(soundfile: "kids.mp3")
+                        }else if self.selections.containsSameElements(as: ramen){
+                            self.isCorrect = true
+                            self.result = "ramen"
+                            Sounds.playSounds(soundfile: "kids.mp3")
+                        }else {
+                            self.result = "belum beruntung"
+                            self.isCorrect = false
+                            Sounds.playSounds(soundfile: "fail.mp3")
+                        }
+                    }
+                    
+                    self.showingAlert = true
+                    self.selections.removeAll()
+                }){
+                    HStack {
+                        Image(uiImage: UIImage(named: "ricecooker.png")!).renderingMode(.original)
+                            .resizable().frame(width: 40,height: 40)
+                            .aspectRatio(contentMode: .fit)
+                        Text("cook")
+                            .fontWeight(.semibold)
+                            .font(.title)
+                    }
+                    .frame(width: 250,height: 40)
+                    .padding()
+                    .foregroundColor(.white)
+                    .background(Color.purple)
+                    .cornerRadius(40)
+                    .padding(.bottom, 20)
+                    
+                }.alert(isPresented: $showingAlert){
+                    Alert(title: Text(self.isCorrect ? "Yeeey!" : "Naaah!"), message: Text(self.isCorrect ? "Your \(self.result ?? "") 🍲 is ready to serve" : "☹️ I dont think those are good combination"), dismissButton: .default(Text(self.isCorrect ? "Okey!" : "Try Again!")))
+                }.padding(.top, 30)
+                
+                
+                
+            }.edgesIgnoringSafeArea(.top)
+            
+            
+        }
+            
+        .navigationBarBackButtonHidden(true)
+    }
+}
+
+struct IntroScreen: View {
+    var body: some View {
+        
+        NavigationView {
+            ZStack{
+                Rectangle()
+                    .foregroundColor(Color(red: 200/255, green: 143/255, blue: 32/255)).edgesIgnoringSafeArea(.all)
+                
+                ScrollView {
+                    Text("RiceCooker Hack").font(.title).padding(.vertical, 30).foregroundColor(Color.white)
+                    
+                    Image(uiImage: UIImage(named: "ricecooker.png")!).renderingMode(.original)
+                        .resizable().frame(width: 100,height: 100)
+                        .aspectRatio(contentMode: .fit).padding(.vertical, 20)
+                    
+                    
+                    Text("Choose THE RIGHT INGREDIENTS \n to create  super easy meals you can make \n in a rice cooker").padding(.bottom, 20).multilineTextAlignment(.center).foregroundColor(Color.white).padding(.horizontal, 10)
+                    
+                    
+                    NavigationLink(destination: ContentView()) {
+                        
+                        Text("LET'S COOK!")
+                            .frame(width: 250,height: 20)
+                            .padding()
+                            .foregroundColor(.white)
+                            .background(Color.purple)
+                            .cornerRadius(40)
+                            .padding(.vertical, 20)
+                    }.buttonStyle(PlainButtonStyle())
+                }
+            }
+            
+            
+        }
+        
+    }
+}
+
+extension Array where Element: Comparable {
+    func containsSameElements(as other: [Element]) -> Bool {
+        return self.count == other.count && self.sorted() == other.sorted()
+    }
+}
+
+
+class Sounds {
+    static var audioPlayer:AVAudioPlayer?
+    static var player:AVAudioPlayer?
+    
+    static func playSounds(soundfile: String) {
+        if let path = Bundle.main.path(forResource: soundfile, ofType: nil){
+            do{
+                audioPlayer = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: path))
+                audioPlayer?.prepareToPlay()
+                audioPlayer?.play()
+            }catch{
+                print("Error")
+            }
+        }
+    }
+    
+    static func playBackSound(soundfile: String){
+        if let path = Bundle.main.path(forResource: soundfile, ofType: nil){
+            do {
+                try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+                try AVAudioSession.sharedInstance().setActive(true)
+                player = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: path))
+                player?.numberOfLoops = -1
+                player?.play()
+            } catch {
+                print("Error")
+            }
+        }
+        
+    }
+}
+
+struct GridStack<Content: View>: View {
+    let rows: Int
+    let columns: Int
+    let content: (Int, Int) -> Content
+    
+    var body: some View {
+        VStack {
+            ForEach(0 ..< rows, id: \.self) { row in
+                HStack {
+                    ForEach(0 ..< self.columns, id: \.self) { column in
+                        self.content(row, column)
+                    }
+                }
+                
+            }
+            
+        }
+    }
+    
+    init(rows: Int, columns: Int, @ViewBuilder content: @escaping (Int, Int) -> Content) {
+        self.rows = rows
+        self.columns = columns
+        self.content = content
+    }
+}
+
+
+
+
+struct CardView: View {
+    
+    var name: String
+    var isSelected: Bool = false
+    var action: () -> Void
+    
+    var body: some View {
+        
+        VStack{
+            Button(action: self.action){
+                Image(uiImage: UIImage(named: "\(name).jpg")!).renderingMode(.original)
+                    .resizable().frame(width: 65,height: 65)
+                    .aspectRatio(contentMode: .fit)
+                    .padding(.all, 5)
+            }
+            .background(Color.white)
+            .border(isSelected ? Color.purple: Color.white, width: 2)
+            .padding(EdgeInsets(top: 0, leading: 2, bottom: 0, trailing: 2))
+            .rotation3DEffect(self.isSelected ? Angle(degrees: 360): Angle(degrees: 0), axis: (x: CGFloat(0), y: CGFloat(10), z: CGFloat(0)))
+            .animation(.default)
+            Text(name).font(.system(size: 12, weight: .heavy, design: .default)).padding(.top, 2).foregroundColor(isSelected ? Color.purple: Color.white)
+            
+        }
+        
+    }
+}
+
+
+PlaygroundPage.current.setLiveView(IntroScreen())
